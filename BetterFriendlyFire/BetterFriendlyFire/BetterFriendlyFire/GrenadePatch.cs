6fbb2dc6a6d7c4add2ae07513433b71921a0dfce
VirtualBrightPlayz <lego.trains.b@gmail.com>
2020-04-14 09:45:55 -0700

    project files

---
+++ BetterFriendlyFire/BetterFriendlyFire/BetterFriendlyFire/GrenadePatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EXILED.Extensions;
+using Grenades;
+using Harmony;
+
+namespace BetterFriendlyFire
+{
+    [HarmonyPatch(typeof(FragGrenade), nameof(FragGrenade.ServersideExplosion))]
+    public class GrenadePatch
+    {
+        public static void Prefix(FragGrenade __instance)
+        {
+            var data = new BFFEventManager.GrenadeThrowData()
+            {
+                team = __instance.NetworkthrowerTeam,
+                Userid = __instance.thrower.GetComponent<ReferenceHub>().GetUserId()
+            };
+            BFFEventManager.grenades = (data);
+        }
+    }
+}
